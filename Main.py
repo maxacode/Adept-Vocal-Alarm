bc032c8d663bbd84d1efa5622b1fb0fade644fe3
Maks Derevencha <maxderevencha@gmail.com>
2022-02-07 00:06:43 -0500

    updateV1.1

--- Main.py
+++ Main.py
@@ -15,30 +15,36 @@ Later Release:
 
 """
  
-import requests
+import requests, os
 
 #Refrences:
 #https://docs.python-requests.org/en/latest/user/quickstart/#passing-parameters-in-urls
 
-app_version = "1.0"
+app_version = 1.0
  
 url = "https://raw.githubusercontent.com/maxacode/Adept-Vocal-Alarm/main/Update.txt"
 r = requests.get(url, allow_redirects=True)
 #print(r.text)
 r_new = r.text.split('\n')
 #print(r_new)
-app_version_pull = r_new[0]
+app_version_pull = float(r_new[0])
 update_link_pull = r_new[1]
+outdated_by = (app_version_pull)-(app_version)
  
 
 if app_version_pull > app_version:
-    print(f"Your Version is Outdated by {int(app_version_pull)-int(app_version)}")
+    print(f"Your Version is Outdated by {str(outdated_by)[:4]}")
     updateQ = input("Update Available, Update Now (Y/N): ")
     if updateQ.lower() == "y":
         print("Downloading Update!")
+        #Renaming old file to _Old Might need a thread since the file is running live. 
+           #os.rename("Main.py","Main_Old.py")
         #Code here to say how long it will take.
         #Download complete then install here. 
- 
+        #Downloading new version
+        r = requests.get(update_link_pull, allow_redirects=True)
+        open(f"MainV"+app_version_pull+".py", 'w').write(r.text)
+
 
 
 
